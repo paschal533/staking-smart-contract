838d8fcc487c18ff84f8c62339cde70103e570c7
paschal533 <okwuosahpaschal@gmail.com>
2022-09-02 03:39:46 +0530

    first commit

--- Staking.sol
+++ Staking.sol
@@ -19,7 +19,7 @@ contract Staking {
     uint public currentPositionId;
     mapping(uint => Position) public positions;
     mapping (address  => uint[]) public positionIdsByAddress;
-    mapping(uint => uint) public tiers;
+    //mapping(uint => uint) public tiers;
     uint[] public lockPeriods;
 
     constructor() payable {
